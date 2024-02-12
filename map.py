5184e6b019a65081af2aeac7e4659b38d512f005
lacozavr <151935499+lacozavr@users.noreply.github.com>
2024-02-12 18:59:32 +0300

    Update map.py
    Передвижение стерелочками и масштаб

--- map.py
+++ map.py
@@ -1,7 +1,8 @@
 import sys
 import requests
 import io
-from PyQt5 import uic, QtGui, QtWidgets
+from PyQt5 import uic, QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
@@ -9,17 +10,62 @@ class Map(QMainWindow):
     def __init__(self):
         super(Map, self).__init__()
         uic.loadUi('E:/map.UI', self)
+        #это
+        self.s = 'Широта:'
+        self.d = 'Долгота:'
+        self.m = 'Масштаб:'
         self.pushButton.clicked.connect(self.search)
+        self.x = ''
+        self.y = ''
+        self.mas_1 = ''
+        self.mas_2 = ''
+        self.shirota.setText(self.s)
+        self.dolgota.setText(self.d)
+        self.aaaa.setText(self.m)
+
 
     def search(self):
-        api_server = f"https://static-maps.yandex.ru/1.x/?ll={self.shir.text()},{self.dolg.text()}" \
-                     f"&spn={self.m_1.text()},{self.m_2.text()}&l=map"
+        if self.pushButton.sender():
+            self.x = self.shir.text()
+            self.y = self.dolg.text()
+            self.mas_1 = self.m_1.text()
+            self.mas_2 = self.m_2.text()
+        #это
+        self.shirota.setText(self.s + f' {self.x}')
+        self.dolgota.setText(self.d + f' {self.y}')
+        self.aaaa.setText(self.m + f' {self.mas_1} X {self.mas_2}')
+        api_server = f"https://static-maps.yandex.ru/1.x/?ll={self.x},{self.y}" \
+                     f"&spn={self.mas_1},{self.mas_2}&l=map"
         response = requests.get(api_server)
         if response:
             with open('map.png', 'wb') as file:
                 file.write(response.content)
             pixmap = QtGui.QPixmap('map.png')
             self.map.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        #это
+        if event.key() == 56:
+            self.y = str(int(self.y) + 1)
+            self.search()
+        elif event.key() == 50:
+            self.y = str(int(self.y) - 1)
+            self.search()
+        elif event.key() == 52:
+            self.x = str(int(self.x) - 1)
+            self.search()
+        elif event.key() == 54:
+            self.x = str(int(self.x) + 1)
+            self.search()
+        elif event.key() == 16777238:
+            self.mas_1 = str(int(self.mas_1) + 1)
+            self.mas_2 = str(int(self.mas_2) + 1)
+            self.search()
+        elif event.key() == 16777239:
+            self.mas_1 = str(int(self.mas_1) - 1)
+            self.mas_2 = str(int(self.mas_2) - 1)
+            self.search()
+
 
 
 def except_hook(cls, exception, traceback):
